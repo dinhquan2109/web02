a3f852c40de0d1697a8f548999e3f928311a22cf
dinhquan2109 <tranquantq7@gmail.com>
2023-11-12 14:27:01 +0700

    web02

--- js.js
+++
@@ -1,9 +0,0 @@
-<script>
-  document.getElementById("searchIcon").addEventListener("mouseenter", function() {
-    document.querySelector(".form-control").style.display = "block";
-  });
-
-  document.querySelector(".form-control").addEventListener("mouseleave", function() {
-    document.querySelector(".form-control").style.display = "none";
-  });
-</script>
